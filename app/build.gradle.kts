d894952014ce3b7f34e427aa21e130ce1060cd09
waxew <4622.ar@gmail.com>
2026-08-26 01:40:54 +0330

    Bump TarikhYar to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ir.tarikhyar.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.4.0"
+        versionCode = 15
+        versionName = "1.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
